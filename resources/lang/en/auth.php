47daeaf92b4a041ea992457cc8fded65d09f3c50
Trung Em <nguyentrungem2002@gmail.com>
2023-12-19 23:22:43 +0700

    Cấu hình project, tích hợp library chat chatify, login ...

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Thông tin bạn nhập không tồn tại trong hệ thống.',
+    'password' => 'Mật khẩu được cung cấp không chính xác.',
+    'throttle' => 'Bạn quá nhiều lần thử đăng nhập. Vui lòng thử lại sau :seconds giây.',
+
+];
