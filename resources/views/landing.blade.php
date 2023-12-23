8dee3720aa7dba70a8bbd718d04257ee6792fa73
Trung Em <nguyentrungem2002@gmail.com>
2023-12-23 17:37:57 +0700

    Cấu hình send email

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Call Space | Landing page</title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
